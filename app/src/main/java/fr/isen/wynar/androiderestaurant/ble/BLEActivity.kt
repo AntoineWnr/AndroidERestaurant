b5d2c6cb0e90d7e06211947772f655deb385d06c
AntoineWnr <antoine.wynar@isen.yncrea.fr>
2022-04-01 17:01:02 +0200

    ble working with rv

--- app/src/main/java/fr/isen/wynar/androiderestaurant/ble/BLEActivity.kt
+++ app/src/main/java/fr/isen/wynar/androiderestaurant/ble/BLEActivity.kt
@@ -1,93 +1,192 @@
 package fr.isen.wynar.androiderestaurant.ble
 
+import BLEAdapter
 import android.Manifest
-import android.bluetooth.le.ScanCallback
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import fr.isen.wynar.androiderestaurant.databinding.ActivityBleBinding
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
-import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import android.os.Bundle
+import android.os.Handler
 import android.util.Log
-import android.widget.ArrayAdapter
-import androidx.annotation.RequiresApi
-import androidx.core.content.PermissionChecker
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import fr.isen.wynar.androiderestaurant.R
+import fr.isen.wynar.androiderestaurant.databinding.ActivityBleBinding
+
 
 class BLEActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityBleBinding
-    private var isScanning = false
-/*
-    private val bleScanner = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult?) {
-            Log.d("ScanDeviceActivity", "onScanResult(): ${result?.device?.address} - ${result?.device?.name}")
-        }
+
+    companion object{
+        private const val ALL_PERMISSION_REQUEST_CODE = 1
+    }
+    private var scanning:Boolean=false
+    private lateinit var binding : ActivityBleBinding
+    private val bluetoothAdapter: BluetoothAdapter ? by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
     }
 
-    private val bluetoothLeScanner: BluetoothLeScanner
-        get() {
-            val bluetoothManager = applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-            val bluetoothAdapter = bluetoothManager.adapter
-            return bluetoothAdapter.bluetoothLeScanner
-        }
+    private var itemsList = ArrayList<ScanResult>()
 
-    class ListDevicesAdapter(context: Context?, resource: Int) : ArrayAdapter<String>(context!!, resource) {
-    }
-*/
+    private lateinit var listBleAdapter: BLEAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBleBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val view = binding.root
+        setContentView(view)
 
-
-        handlePlayPauseButton()
-        Log.d("ScanDeviceActivity", "onCreate()")
-
-    }
-    /*
-    @RequiresApi(Build.VERSION_CODES.M)
-    override fun onStart() {
-        Log.d("ScanDeviceActivity", "onStart()")
-        super.onStart()
-        when (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
-            PackageManager.PERMISSION_GRANTED -> bluetoothLeScanner.startScan(bleScanner)
-            else -> requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode) {
-            1 -> when (grantResults) {
-                intArrayOf(PackageManager.PERMISSION_GRANTED) -> {
-                    Log.d("ScanDeviceActivity", "onRequestPermissionsResult(PERMISSION_GRANTED)")
-                    bluetoothLeScanner.startScan(bleScanner)
+        when {
+            bluetoothAdapter?.isEnabled == true ->
+                binding.imgPlayBle.setOnClickListener {
+                    startLeScanBLEWithPermission(!scanning)
+                    Log.d("TEST2", "ICI")
                 }
-                else -> {
-                    Log.d("ScanDeviceActivity", "onRequestPermissionsResult(not PERMISSION_GRANTED)")
-                }
-            }
-            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        }
-    }
-*/
-    private fun handlePlayPauseButton(){
-        binding.buttonStart.setOnClickListener {
-            if (isScanning) {
-                binding.progressBarBLE.isIndeterminate = false
-                binding.textViewBLEState.text = "Recherche d'appareil bluetooh"
-            } else {
-                binding.progressBarBLE.isIndeterminate = true
-                binding.textViewBLEState.text = "Lancer la recherche"
+            bluetoothAdapter != null ->
+                askBluetoothPermission()
+            else -> {
+                displayNoBleAvailable()
             }
         }
+        val recyclerBle: RecyclerView = binding.ListBleScan
+        listBleAdapter = BLEAdapter(itemsList, BLEAdapter.OnClickListener { item ->
+            onListBleClick(item)
+        })
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerBle.layoutManager = layoutManager
+        recyclerBle.adapter = listBleAdapter
     }
-/*
-    override fun onStop() {
-        Log.d("ScanDeviceActivity", "onStop()")
+
+
+    @SuppressLint("MissingPermission")
+    private fun onListBleClick(item: ScanResult) {
+        if(item.device.name.isNullOrEmpty()){
+            Toast.makeText(this@BLEActivity, "Unknown name", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this@BLEActivity, item.device.name.toString(), Toast.LENGTH_SHORT).show()
+        }
+        val intent = Intent(this, BLEActivity::class.java)
+        intent.putExtra("Device", item)
+        startActivity(intent)
+    }
+
+    override fun onStop(){
         super.onStop()
-        bluetoothLeScanner.stopScan(bleScanner)
+        startLeScanBLEWithPermission(false)
     }
-*/
+
+    private fun startLeScanBLEWithPermission(enable: Boolean){
+        if (checkAllPermissionGranted()) {
+            Log.d("startScan", "ICI2")
+            startLeScanBLE(enable)
+        }else{
+            ActivityCompat.requestPermissions(this, getAllPermissions() ,ALL_PERMISSION_REQUEST_CODE)
+            Log.d("startScan2", "ICI3")
+        }
+    }
+
+    private fun checkAllPermissionGranted(): Boolean {
+        return getAllPermissions().all { permission ->
+            ActivityCompat.checkSelfPermission(
+                this,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun getAllPermissions(): Array<String> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_CONNECT
+            )
+        } else {
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLeScanBLE(enable: Boolean) {
+
+        bluetoothAdapter?.bluetoothLeScanner?.apply {
+            if(enable) {
+                val handler = Handler(mainLooper)
+                handler.postDelayed({
+                    scanning=false// stop scanning here
+                    binding.imgPlayBle.setImageResource(R.drawable.ic_baseline_arrow_right_24)
+                    binding.textViewBLEState.text = "Scanning"
+                    binding.progressBarBLE.isIndeterminate = false
+                    stopScan(scanCallBack)
+                }, 15000)
+                scanning=true
+                startScan(scanCallBack)
+            }else{
+                scanning=false
+                stopScan(scanCallBack)
+            }
+            btnPlayClick()
+        }
+    }
+
+    private val scanCallBack = object : ScanCallback(){
+        override fun onScanResult(callBackType : Int, result:ScanResult){
+            Log.d("BLEScanActivity","${result.device.address} - ${result.rssi}")
+            addToList(result)
+
+        }
+    }
+
+    private fun addToList(res:ScanResult){
+        val index:Int = itemsList.indexOfFirst{ it.device.address==res.device.address }
+        if(index == -1){
+            itemsList.add(res)
+        }else{
+            itemsList[index]=res
+        }
+        itemsList.sortBy { kotlin.math.abs(it.rssi) }
+        listBleAdapter.notifyDataSetChanged()
+    }
+
+    private fun displayNoBleAvailable() {
+        binding.imgPlayBle.isVisible = false
+        binding.textViewBLEState.text="Lancer le scan"
+        binding.progressBarBLE.isIndeterminate=true
+    }
+
+    private fun btnPlayClick() {
+        binding.imgPlayBle.isVisible = true
+        if (scanning) {
+            binding.imgPlayBle.setImageResource(R.drawable.ic_baseline_app_blocking_24)
+            binding.textViewBLEState.text = "Chargement"
+            binding.progressBarBLE.isIndeterminate = true
+        } else {
+            binding.imgPlayBle.setImageResource(R.drawable.ic_baseline_arrow_right_24)
+            binding.textViewBLEState.text = "Lancer le scan"
+            binding.progressBarBLE.isIndeterminate = false
+        }
+    }
+
+    private fun askBluetoothPermission() {
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            startActivityForResult(enableBtIntent, ALL_PERMISSION_REQUEST_CODE)
+        }
+    }
 }
--- app/src/main/java/fr/isen/wynar/androiderestaurant/ble/BLEAdapter.kt
+++ app/src/main/java/fr/isen/wynar/androiderestaurant/ble/BLEAdapter.kt
@@ -1,4 +1,41 @@
-package fr.isen.wynar.androiderestaurant.ble
 
-class BLEAdapter {
+import android.annotation.SuppressLint
+import android.bluetooth.le.ScanResult
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import fr.isen.wynar.androiderestaurant.databinding.CellBleBinding
+
+internal class BLEAdapter(private var itemsList: MutableList<ScanResult>, private val onClickListener: OnClickListener) : RecyclerView.Adapter<BLEAdapter.MyViewHolder>() {
+
+    private lateinit var binding: CellBleBinding
+
+    internal inner class MyViewHolder(binding: CellBleBinding) : RecyclerView.ViewHolder(binding.root) {
+        val mac=binding.mac
+        val name=binding.name
+        val rssi=binding.rssi
+    }
+    @NonNull
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        binding = CellBleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+    @SuppressLint("MissingPermission")
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = itemsList[position]
+        holder.mac.text =  item.device.address
+        holder.name.text = if(item.device.name == null){"Unknow Name"}else{item.device.name}
+        holder.rssi.text = "${item.rssi} Db"
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(item)
+        }
+    }
+    override fun getItemCount(): Int {
+        return itemsList.size
+    }
+
+    class OnClickListener(val clickListener: (item: ScanResult) -> Unit) {
+        fun onClick(item: ScanResult) = clickListener(item)
+    }
 }
